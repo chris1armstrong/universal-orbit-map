adf46221dcfdc7345bcc261b9cd5b17e8e151a87
Chris Armstrong <chrisjarmstrong97@gmail.com>
2020-03-18 17:37:06 +1100

    read input, build satellites dict

--- orbit_map.py
+++ orbit_map.py
@@ -1,7 +1,13 @@
-from satellite import Satellite
+from collections import defaultdict
 
 if __name__ == "__main__":
 
-    satellites = []
-    
-    print("SETUP")
+    satellites = defaultdict(list)
+    dummy = 0
+    with open('input.txt','r') as f:
+        for line in f:
+            center,orbiter = line.strip().split(')')
+            satellites[orbiter].append(center)
+    f.close()
+    for i in satellites:
+        print(i + ': ' + str(satellites[i]))
